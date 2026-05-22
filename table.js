a4cc7b64e15d7f81469d28792cb42d2e4799b7c2
Pratik-acg <pratik241545@arkajainuniversity.ac.in>
2026-05-22 12:23:01 +0530

    a simple calculator for tables using loop

---
+++ table.js
@@ -0,0 +1,16 @@
+// const table=1
+// console.log(table*1)
+// console.log(table*2)
+// console.log(table*3)
+// console.log(table*4)
+// console.log(table*5)
+// console.log(table*6)
+// console.log(table*7)
+// console.log(table*8)
+// console.log(table*9)
+// console.log(table*10)
+
+const table=20
+for(let i=1;i<=10;i++){
+    console.log(table*i);
+}   
