4c0c09b2c18a3282aa52ad73dc536dfd10f70a54
Pratik-acg <pratik241545@arkajainuniversity.ac.in>
2026-05-22 12:00:15 +0530

    Variablr filr to learn about let and const

---
+++ variable.js
@@ -0,0 +1,8 @@
+let name="Pratik";
+const dob="01/01/2000"; 
+//console.log("My name is " + name + " and I was born on " + dob + ".");
+console.log(name,dob);
+name="Rahul";
+console.log(name);
+//dob="02/02/2000"; 
+console.log(dob);
