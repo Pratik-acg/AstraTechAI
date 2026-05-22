d22c9403cdb67de5c332f0560da5b3b131637d17
Pratik-acg <pratik241545@arkajainuniversity.ac.in>
2026-05-22 12:37:01 +0530

    Practice Questions

---
+++ even.js
@@ -0,0 +1,9 @@
+//Write a program to print all even numbers from 1 to 100.
+//Write a program to print all odd numbers from 1 to 100.
+//Write a program to print all prime numbers from 1 to 100.
+
+for(let i=1;i<=100;i++){
+    if(i%2==0){
+        console.log(i);
+    }   
+}
---
+++ prime.js
@@ -0,0 +1,11 @@
+for(let i=1;i<=100;i++){
+    let c=0
+    for(let j=1;j<=i;j++){
+        if(i%j==0){
+            c++;
+        }
+    }    
+    if(c==2){
+        console.log(i);
+    }
+}
